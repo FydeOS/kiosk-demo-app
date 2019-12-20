80feb0ee29f11411017375531775e289178a10bf
yang <yang@fydeos.io>
2019-12-20 18:58:27 +0800

    change url to https://periodex.co/chromium

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.config = {
    "model_": "AppConfig",
    "id": 1,
    "appName": "FydeOS Kiosk Test App",
-   "homepage": "https://www.towergame.app",
+   "homepage": "https://periodex.co/chromium",
    "sessionDataTimeoutTime": 5,
    "sessionTimeoutTime": 5,
    "kioskEnabled": true
